a021db173c909bf735fe96e62ec93be15b815b76
Jed Rembold <jedediah.r@gmail.com>
2022-09-01 20:29:31 -0700

    Initial commit

---
+++ Problem3.sql
@@ -0,0 +1,8 @@
+/*
+Write out a possible CREATE TABLE statement below that would enable you to have the data
+necessary to answer all the provided questions. Be sure to include a comment for each
+column explaining why you chose the data type that you did for that column.
+*/
+
+CREATE TABLE
+
---
+++ Problem4.sql
@@ -0,0 +1,98 @@
+/*
+Problem 4
+
+For each of the parts below, I have provided you space to both add your answer to the question,
+to describe how you took the result of your query to answer the question, and to provide the
+query itself.
+*/
+
+
+----------------------------------------------------------------------
+-- Part A
+-- What is the earliest formed band in the data set?
+----------------------------------------------------------------------
+-- ANSWER:
+-- Description:
+-- Query below
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------
+-- Part B
+-- How many rows are duplicated?
+----------------------------------------------------------------------
+-- ANSWER:
+-- Description:
+-- Query below
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------
+-- Part C
+-- How many bands have "death" appearing in their name?
+----------------------------------------------------------------------
+-- ANSWER:
+-- Description:
+-- Query below
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------
+-- Part D
+-- Second most popular German "Viking folk" style band?
+----------------------------------------------------------------------
+-- ANSWER:
+-- Description:
+-- Query below
+
+
+
+
+
+
+
+
+
+
+
+----------------------------------------------------------------------
+-- Part E
+-- Number of bands which formed and split in the same year?
+----------------------------------------------------------------------
+-- ANSWER:
+-- Description:
+-- Query below
+
+
+
+
+
+
+
+
+
+
